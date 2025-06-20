cf8c7d3932c823e041ba52be3d4153bd072effd7
imam0321 <imam.hossain0321@gmail.com>
2025-06-20 23:06:10 +0600

    library management backend project setup

---
+++ src/server.ts
@@ -0,0 +1,26 @@
+import dotenv from "dotenv";
+import { Server } from "http";
+import app from "./app";
+import mongoose from "mongoose";
+
+dotenv.config();
+let server: Server;
+
+const PORT = process.env.PORT || 5000;
+
+async function main() {
+  try {
+    if (!process.env.MONGODB_URL) {
+      throw new Error("MONGODB_URL is not defined in .env");
+    }
+    await mongoose.connect(process.env.MONGODB_URL);
+    console.log("Mongoose Connected!");
+    server = app.listen(PORT, () => {
+      console.log(`App is listening on port ${PORT}`);
+    });
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+main();
